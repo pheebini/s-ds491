9306e4794e7469103111ae6875797c58cf6d1b43
Phoebe Liu <63767719+pheebini@users.noreply.github.com>
2022-12-07 13:53:29 -0500

    Add files via upload
    These files were used to scrape Yahoo Finance data (by saving HTMLs to my disk)

---
+++ turt.py
@@ -0,0 +1,13 @@
+import requests
+def foo(url_dataframe):
+    url_test = url_dataframe["URL"]
+    ticker_test = url_dataframe["Ticker"]
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+    response = requests.get(url_test, headers=headers)
+    content = response.content.decode("utf-8")
+
+    filename = f"/Users/phoebeliu/Documents/SDS_Senior_Project/YahooFinance_Profile_HTMLs/{ticker_test}.html"  # folder
+    print(filename)
+    with open(filename, 'w') as f:
+        f.write(content)
